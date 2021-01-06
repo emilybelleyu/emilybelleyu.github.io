f783eebb1cfa695000a899f72762fc6bbd9d6669
emilybelleyu <emilybelleyu@users.noreply.github.com>
2021-01-06 12:52:09 +0000

    deployed via Gatsby Publish Action 🎩 for 4e886b7a092da80e5d3c67e7ce16eceda66366e4

---
+++ component---src-pages-404-js-93ae595c7757989accdf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),l=t.n(a);e.default=function(){return l.a.createElement("div",null,l.a.createElement("p",null,"404"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-93ae595c7757989accdf.js.map
